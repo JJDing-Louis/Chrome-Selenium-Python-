6fd0e5f3cc2ccf6125cb0a134b32a862ee7671b1
xujpvuxu <xujpvuxu2332@gmail.com>
2020-05-09 12:07:21 +0800

    Signed-off-by: xujpvuxu <xujpvuxu2332@gmail.com> fix

--- sel.py
+++ sel.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
 browser = webdriver.Chrome()
-browser.get('https://ppt.cc/f9GtSx')
+use=input("帳號:")
+psw=input("密碼")
 
-a=browser.find_element_by_xpath('/html/body/div/form/center/b/font/input[1]')
-a.click()
+browser.get('https://www.eyny.com/member.php?mod=logging&action=login')
 
-a=browser.find_element_by_name('p')
-a.send_keys('0507')
+a=browser.find_element_by_name('username')
+a.send_keys(use)
+
+a=browser.find_element_by_name('password')
+
+a.send_keys(psw)
 
 a.submit()
